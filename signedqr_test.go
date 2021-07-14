86849d9a97b430f4da787a7b402c6547b4289bdd
Yoshiyuki Harada <noreply@kawauso-lab.org>
2021-07-14 22:02:20 +0900

    Enhanced tests.

--- signedqr_test.go
+++ signedqr_test.go
@@ -10,7 +10,7 @@ import (
 	"time"
 )
 
-func TestExampleSuccess(t *testing.T) {
+func TestSuccess(t *testing.T) {
 	ctx := time.Now().Format("2006-0102-150405")
 
 	GenerateKeyPair(".", ctx)
@@ -32,6 +32,37 @@ func TestExampleSuccess(t *testing.T) {
 	}
 	res, err := Decode(data, fmt.Sprintf("./%s.pub", ctx))
 	if err != nil {
+		t.Fatal(err)
+	}
+	println(res)
+}
+
+func TestVerifyWithAnotherKey(t *testing.T) {
+	ctx_a := time.Now().Format("2006-0102-150405") + "-A"
+	ctx_b := time.Now().Format("2006-0102-150405") + "-B"
+
+	GenerateKeyPair(".", ctx_a)
+	GenerateKeyPair(".", ctx_b)
+
+	qr := Encode("Sample string", fmt.Sprintf("./%s.key", ctx_a))
+	file, err1 := os.Create(fmt.Sprintf("%s.png", ctx_a))
+	if err1 != nil {
+		t.Fatal(err1)
+	}
+	buf := new(bytes.Buffer)
+	_ = png.Encode(buf, qr)
+	_, err2 := file.Write(buf.Bytes())
+	if err2 != nil {
+		t.Fatal(err2)
+	}
+	data, err := ioutil.ReadFile(fmt.Sprintf("%s.png", ctx_a))
+	if err != nil {
+		t.Fatal(err)
+	}
+	res, err := Decode(data, fmt.Sprintf("./%s.pub", ctx_b))
+
+	// It would be strange if I didn't get an error.
+	if err == nil {
 		t.Fatal(err)
 	}
 	println(res)
